22e2f4e85a6500be5c5ef4eadf8aba7627a941ff
ashdevelops <51748995+ashdevelops@users.noreply.github.com>
2022-08-19 01:14:41 +0100

    Moved source files to subfolders

--- source/interface/Kelpie/KelpieUploader.h
+++
@@ -1,9 +0,0 @@
-#import <UIKit/UIKit.h>
-#import <CoreLocation/CoreLocation.h>
-#import <Foundation/Foundation.h>
-#import <objc/runtime.h>
-
-@interface KelpieUploader: NSObject
-+(void)saveImageToServer:(UIImage*)image senderUsername:(NSString *)senderUsername receiverUsername:(NSString *)receiverUsername;
-+(void)saveVideoToServer:(NSString*)filePath senderUsername:(NSString *)senderUsername receiverUsername:(NSString *)receiverUsername;
-@end
