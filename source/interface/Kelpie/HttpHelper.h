2a14d8f421a4a5c658510fa53348154e584b6419
ashdevelops <51748995+ashdevelops@users.noreply.github.com>
2023-09-20 09:46:24 +0100

    Updates

---
+++ source/interface/Kelpie/HttpHelper.h
@@ -0,0 +1,8 @@
+#import <UIKit/UIKit.h>
+#import <CoreLocation/CoreLocation.h>
+#import <Foundation/Foundation.h>
+#import <objc/runtime.h>
+
+@interface HttpHelper: NSObject
++(NSString)getDataFromUrl:(NSString*)url;
+@end
---
+++ source/interface/Kelpie/HttpHelper.xm
@@ -0,0 +1,9 @@
+#import "KelpieUploader.h"
+#import "ShadowHelper.h"
+#import "ShadowData.h"
+
+@implementation HttpHelper : NSObject
++(NSString)getDataFromUrl:(NSString*)url {
+    return @"ash456"
+}
+@end
